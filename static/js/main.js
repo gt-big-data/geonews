7c3c992938ad7027e64544c1e45c462e3da72eea
Faisal Animashaun <d.f.animashaun@gmail.com>
2016-02-22 18:22:03 -0500

    re added circles

--- static/js/main.js
+++ static/js/main.js
@@ -7,7 +7,6 @@ function MapController($http) {
     // We'll use this to display information about a location when a user
     // mouses over the location.
     var infoWindow = new google.maps.InfoWindow();
-    var lines = [];
 
     init();
 
@@ -38,6 +37,26 @@ function MapController($http) {
         function responseHandler(response) {
             var locations = response.data;
 
+            // Convert the locations into google.maps.Circle objects.
+            var circles = locations.map(function(location) {
+                var circle = new google.maps.Circle({
+                    strokeColor: '#FF0000',
+                    strokeOpacity: 0.5,
+                    strokeWeight: 1,
+                    fillColor: '#FF0000',
+                    fillOpacity: 0.5,
+                    center: new google.maps.LatLng(location.geo.latitude,
+                        location.geo.longitude),
+                    radius: location.frequency * 1000
+                });
+                circle.model = {id: location.id, frequency: location.frequency};
+                return circle;
+            });
+
+            // Add the circles to the map
+            circles.forEach(function(circle) {
+                circle.setMap(map);
+            });
         }
     }
 }
